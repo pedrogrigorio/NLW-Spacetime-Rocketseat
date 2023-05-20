f580c02427fb7311112a6937fc3488fa3bd837bd
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-05-20 12:23:15 -0300

    github authentication

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
+import { cookies } from 'next/headers'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Copyright } from '@/components/Copyright'
 import { SignIn } from '@/components/SignIn'
 import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -14,7 +18,7 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
 
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
