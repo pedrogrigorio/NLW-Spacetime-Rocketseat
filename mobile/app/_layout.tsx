3852d933fd474f424b57996dd82e4659c102f210
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-05-20 23:10:19 -0300

    new memories screen

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,61 @@
+import { styled } from 'nativewind'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import { ImageBackground } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SplashScreen, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+
+const StyledStripes = styled(Stripes)
+
+export default function Layout() {
+  const [isAuthenticated, setIsAuthenticated] = useState<null | boolean>(null)
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      console.log(!!token)
+      setIsAuthenticated(!!token)
+    })
+  }, [])
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return <SplashScreen />
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <StatusBar style="light" translucent />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
+    </ImageBackground>
+  )
+}
