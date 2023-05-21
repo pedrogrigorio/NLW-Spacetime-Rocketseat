d4214036b4d4d1f9a617e5e0f5866f0011e393f6
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-05-20 23:33:45 -0300

    files upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRotes } from './routes/auth'
+import { uploadRoates } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -16,6 +26,7 @@ app.register(jwt, {
 })
 
 app.register(authRotes)
+app.register(uploadRoates)
 app.register(memoriesRoutes)
 
 app
